aa4900d2f2c9ec111e6e79067e6f63df967381f2
nazmul.biswass <nazmul.islam.lotif@gmail.com>
2026-02-07 10:55:20 +0600

    projects navbar issue fixed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ const navLinks = [
   { name: 'About', href: '/#about' },
   { name: 'Skills', href: '/#skills' },
   { name: 'Experience', href: '/#experience' },
-  { name: 'Projects', href: '/#projects' },
+  { name: 'Projects', href: '/projects' },
   { name: 'Education', href: '/#education' },
   { name: 'Contact', href: '/#contact' },
 ]
