5ea44f45ab2698925056d56d9ce97c180f3a2529
nazmul.biswass <nazmul.islam.lotif@gmail.com>
2026-02-07 11:20:57 +0600

    projects navbar issue fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import { Routes, Route } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import { ThemeProvider } from './context/ThemeContext'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Projects from './pages/Projects'
 import Footer from './components/Footer'
 
+function ScrollToTop() {
+  const { pathname } = useLocation()
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+  return null
+}
+
 function App() {
   return (
     <ThemeProvider>
       <div className="min-h-screen bg-white dark:bg-dark-900 transition-colors duration-300 overflow-x-hidden">
+        <ScrollToTop />
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
